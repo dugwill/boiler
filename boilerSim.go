489817f2636a9803a9126f4e40a1b510d351a439
Douglas Will <douglaswi1@comcast.net>
2022-12-12 16:23:57 -0700

    lots of chabges

--- boilerSim.go
+++ boilerSim.go
@@ -4,52 +4,34 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"time"
 
-	"github.com/dugwill/boiler/modbusRTU"
-	"github.com/tarm/serial"
+	"github.com/dugwill/modbus"
+	"github.com/dugwill/triangleTube"
 )
 
 func main() {
 
-	connection, err := ConnectRTU("COM6", 38400)
+	port := `/dev/ttyUSB1`
+	recChan := make(chan []byte)
+
+	t := triangleTube.NewBoiler(1)
+
+	var rtu modbus.Connection = &modbus.RTU{SerialDevice: port, BaudRate: 38400, StopBits: 1}
+
+	err := rtu.Connect()
 	if err != nil {
-		log.Fatalln("Error opening port")
+		log.Fatalln("Error opening serial port: ", err)
+	} else {
+		log.Println("Serial connetion successful")
 	}
-	defer connection.Close()
+	defer disconnect(rtu)
 
-	buf := make([]byte, 128)
+	go rtu.Read(recChan)
 
-	byteCnt, response, err := readCommand(connection, buf)
-	if err != nil {
-		log.Println("Error reading input", err)
+	for {
+		response := <-recChan
+		log.Printf("Received %d bytes: %x\n", len(response), response)
 	}
-
-	log.Printf("Received %d bytes: %x\n", byteCnt, response)
-
-	data := []byte{0x03, 0x36, 0x04, 0x36}
-
-	adu := modbusRTU.GenerateResponse(0x01, 0x04, data)
-
-	w, err := connection.Write(adu)
-	if err != nil {
-		log.Print("Error writing to Serial: ", err)
-	}
-	log.Printf("Wrote %d Bytes: %x", w, adu)
-}
-
-// ConnectRTU attempts to access the Serial Device for subsequent
-// RTU writes and response reads from the modbus slave device
-func ConnectRTU(serialDevice string, baudRate int) (io.ReadWriteCloser, error) {
-	conf := &serial.Config{Name: serialDevice, Baud: baudRate, StopBits: 2}
-	ctx, err := serial.OpenPort(conf)
-	ctx.Flush()
-	return ctx, err
-}
-
-// DisconnectRTU closes the underlying Serial Device connection
-func DisconnectRTU(ctx io.ReadWriteCloser) {
-	ctx.Close()
 }
 
 func readCommand(connection io.ReadWriteCloser, buf []byte) (byteCnt int, response []byte, err error) {
@@ -57,28 +39,8 @@ func readCommand(connection io.ReadWriteCloser, buf []byte) (byteCnt int, respon
 	var rBytes []byte
 
 	for {
-		n, err := connection.Read(buf)
-		if err != nil {
-			log.Println("Error reading from port: ", err)
-			return 0, nil, err
-		}
 
-		log.Printf("First Read: %d bytes, %x\n", n, buf[:n])
-		if n < 8 {
-			byteCnt += n
-			rBytes = make([]byte, n)
-			copy(rBytes, buf)
-			time.Sleep(100 * time.Millisecond)
-			n, err = connection.Read(buf)
-			if err != nil {
-				log.Println("Error reading from port: ", err)
-			}
-			log.Printf("Second Read: %d bytes, %x\n", n, buf[:n])
-			byteCnt += n
-			rBytes = append(rBytes, buf[:n]...)
-		}
-
-		logMsg := fmt.Sprintf("Read %d bytes: ", byteCnt)
+		logMsg := fmt.Sprintf("Read %d bytes test: ", byteCnt)
 		for i := 0; i < byteCnt; i++ {
 			logMsg = logMsg + fmt.Sprintf("%x ", rBytes[i])
 		}
@@ -88,8 +50,15 @@ func readCommand(connection io.ReadWriteCloser, buf []byte) (byteCnt int, respon
 			log.Printf("Too many bytes read: %d. Flushing buffer.", byteCnt)
 			continue
 		} else {
-			break
+			//validateChecksum
 		}
 	}
-	return
+	// return buf without checksum
+	return byteCnt, buf[:6], nil
+}
+
+func disconnect(rtu modbus.Connection) {
+	log.Println("shutting down.")
+	rtu.Disconnect()
+
 }
